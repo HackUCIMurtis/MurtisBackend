739cd6b584f4ba28d59e6e83a246e2362549c23e
akhild-uci <akhil.dhanala@gmail.com>
2021-01-29 23:30:48 -0800

    boilerplate

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+// environment file
+require("dotenv").config();
+
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+
+// Load credentials from firebase
+// const serviceAccount = require('./serviceAccountKey.json');
+const { query } = require('express');
+
+// Load Twilio SMS
+const accountSid = "";
+const authToken = "";
+//const client = require('twilio')(accountSid, authToken);
+
+// Create the firebase connection
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+};
+firebase.initializeApp(firebaseConfig);
+
+// Initialize our DB
+const db = firebase.firestore();
+
+
+app.listen(port, () => {
+    console.log(`Murtis API listening on port ${port}`);
+})
